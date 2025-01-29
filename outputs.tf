e87c17de8e8eace6c46a42dd9cf58d9151e807b0
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-01-29 10:40:50 -0800

    Add repository_url output, configure bumpversion

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "repository_arn" {
   description = "ECR repository ARN."
   value       = aws_ecr_repository.repo.arn
 }
+
+output "repository_url" {
+    description = "ECR repository URL."
+    value       = aws_ecr_repository.repo.repository_url
+}
