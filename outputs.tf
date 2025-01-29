1019b6a790eebc86a2a25ec159901f286d1b9e66
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-01-29 10:55:46 -0800

    Fix formatting

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,6 @@ output "repository_arn" {
 }
 
 output "repository_url" {
-    description = "ECR repository URL."
-    value       = aws_ecr_repository.repo.repository_url
+  description = "ECR repository URL."
+  value       = aws_ecr_repository.repo.repository_url
 }
