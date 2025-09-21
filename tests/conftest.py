bfda00e6b60547861d57a9c5f0125a72d18008ea
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-09-21 10:42:26 +0200

    Fix logging imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,10 @@
-from pprint import pformat
-from textwrap import dedent
-from time import sleep
-
-import pytest
 import logging
-from os import path as osp
 
-from infrahouse_toolkit.logging import setup_logging
-from infrahouse_toolkit.terraform import terraform_apply
+from infrahouse_core.logging import setup_logging
 
 DEFAULT_PROGRESS_INTERVAL = 10
 TEST_TIMEOUT = 3600
 TRACE_TERRAFORM = False
-UBUNTU_CODENAME = "jammy"
 
 LOG = logging.getLogger(__name__)
 TERRAFORM_ROOT_DIR = "test_data"
